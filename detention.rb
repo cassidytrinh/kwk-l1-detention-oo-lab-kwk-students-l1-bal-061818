c0bd559e1fa6ef8cdb5be5a4d0a28d8b5e9d78cf
cassidytrinh <cassidytrinh@gmail.com>
2018-06-23 18:05:19 +0000

    automatically backed up by learn

--- detention.rb
+++ detention.rb
@@ -1,3 +1,33 @@
 #Code your detention class here
 class Detention
   attr_accessor :location, :time, :grumpy_teacher, :activity, :students
+
+  def location=(location)
+    @location = location
+  end
+
+  def location
+    @location
+  end
+
+  def time=(time)
+    @time = time
+  end
+
+  def time
+    @time
+  end
+
+  def time
+    @time
+  end
+
+  def grumpy_teacher=(grumpy_teacher)
+    @grumpy_teacher = grumpy_teacher
+  end
+
+  def grumpy_teacher
+    @grumpy_teacher
+  end
+
+  
