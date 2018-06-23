0421ba28fe206edf1a367521be303e1dddca4e8c
cassidytrinh <cassidytrinh@gmail.com>
2018-06-23 18:11:20 +0000

    Done.

--- detention.rb
+++ detention.rb
@@ -1,1 +1,57 @@
 #Code your detention class here
+class Detention
+  attr_accessor :location, :time, :grumpy_teacher, :activity, :students
+
+  def initialize
+    @activity = "collective punishment"
+    @students = []
+  end
+
+  def location=(location)
+    @location = location
+  end
+
+  def location
+    @location
+  end
+
+  def time=(time)
+    @time = time
+  end
+
+  def time
+    @time
+  end
+
+  def time
+    @time
+  end
+
+  def grumpy_teacher=(grumpy_teacher)
+    @grumpy_teacher = grumpy_teacher
+  end
+
+  def grumpy_teacher
+    @grumpy_teacher
+  end
+
+  def activity=(activity)
+    @activity = activity
+  end
+
+  def activity
+    @activity
+  end
+
+  def students
+    @students
+  end
+
+  def add_student(student)
+    @students.push(student)
+  end
+
+  def remove_student(student)
+    @students.delete(student)
+  end
+end
